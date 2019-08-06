4e033e018e7998a6aacd3efce515b47feb2ae0fe
Peter Nielson <computergeekpete@gmail.com>
2019-08-05 22:44:22 -0500

    Changed configurations and plugins according to Babel and webpack docos

--- webpack.config.js
+++ webpack.config.js
@@ -1,25 +1,24 @@
 var webpack = require('webpack');  
 module.exports = {  
-  entry: [
-    "./react-files/app.js"
-  ],
-  output: {
-    path: __dirname + '/static',
-    filename: "bundle.js"
-  },
-  module: {
-    rules: [
-      {
-        test: /\.(js|jsx)?$/,
-        loader: 'babel-loader',
-        query: {
-          presets: ['env', 'react']
-        },
-        exclude: /node_modules/
-      }
-    ]
-  },
-  plugins: []
+    entry: [
+        "./react-files/app.jsx"
+    ],
+    output: {
+        path: __dirname + '/static',
+        filename: "bundle.js"
+    },
+    module: {
+        rules: [
+            {
+                test: /\.(js|jsx)?$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: 'babel-loader'
+                }
+            }
+        ]
+    },
+    plugins: []
 };
 
 /*
