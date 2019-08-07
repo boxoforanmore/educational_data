3e3d583fb4aa4c11a47b72351b4c717fdaf91b09
Peter Nielson <computergeekpete@gmail.com>
2019-08-06 19:45:12 -0500

    Added environment variables for API key

--- app.py
+++ app.py
@@ -1,11 +1,18 @@
 from flask import Flask, render_template
+import os
+
+basedir = os.path.abspath(os.path.dirname(__file__))
+
+class Config():
+    SECRET_KEY = os.environ.get('SECRET_KEY') or 'a long string with jib jab wallygog give a dog a cone'
+    API_KEY = os.environ.get('API_KEY') or exit('Please set a global environnment API key')
 
 app = Flask(__name__)
-
+app.config.from_object(Config)
 
 @app.route('/')
 def home():
-    return render_template('index.html')
+    return render_template('index.html', API_KEY=Config.API_KEY)
 
 
 if __name__ == '__main__':
