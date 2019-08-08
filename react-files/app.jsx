7500ed6ed0742dc87c9a25274db2ba79aeab7888
Peter Nielson <computergeekpete@gmail.com>
2019-08-08 11:35:30 -0500

    Cleaned up main  component

--- react-files/app.jsx
+++ react-files/app.jsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { DataHandler } from './DataHandler.jsx';
-//import { ErrorHandler } from './ErrorHandler.jsx';
 
 const API_URL = "https://api.data.gov/ed/collegescorecard/v1/schools/?school.operating=1&2015.academics.program_available.assoc_or_bachelors=true&2015.student.size__range=1..&school.degrees_awarded.predominant__range=1..3&school.degrees_awarded.highest__range=2..4&id=240444&api_key=";
 
@@ -47,20 +46,6 @@ class MainApp extends React.Component {
                 <DataHandler data={ this.state.results } isLoaded={ this.state.isLoaded } />
             );
         }
-        
-        /*
-        console.log('help me');
-        if (this.state.error) {
-            return <ErrorHandler />;
-        }
-        else {
-            return (
-
-                <DataHandler data={this.state.data} isLoaded={this.state.isLoaded} />
-
-            );
-        }
-        */
     }
 }
 
